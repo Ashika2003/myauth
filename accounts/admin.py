d3fb9c7ee865246ec841c859f089242b9bb1282a
Ashika2003 <113971898+Ashika2003@users.noreply.github.com>
2025-01-26 22:35:09 +0530

    Added Assignment

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User, UserProfile
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_active', 'is_staff', 'is_superuser']
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'address']
